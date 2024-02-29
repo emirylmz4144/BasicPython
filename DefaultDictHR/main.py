1ca8b76ade6d4b63d5d09e5a8886e46cae43cd5f
emirylmz4144 <emiryilmaz4144@gmail.com>
2024-02-29 22:41:17 +0300

    command line added in DefaultDictHR project

--- DefaultDictHR/main.py
+++ DefaultDictHR/main.py
@@ -1,8 +1,10 @@
-from collections import defaultdict
+from collections import defaultdict #DefaultDict yapısı import edildi
 
-n,m=map(int,input().split())
+n,m=map(int,input().split()) # n ve m değerleri ard arda girileceğinden split edildi
 
-A=defaultdict(list)
+A=defaultdict(list)#içi liste olacak defaultDict oluşturuldu
+
+#Liste elemanının (harfin) hangi yerlerde oldukları value değerine ekleniyor
 for i in range(n):
     word=input()
     A[word].append(i)
@@ -12,13 +14,13 @@ for i in range(m):
     word=input()
     B.append(word)
 
-for word in B:
-    if word in A:
-        for location in A[word]:
-            print(location+1,end=" ")
+for word in B:# B içindeki her harfin
+    if word in A:#A içinde key olarak tutulan her harfin içinde
+        for location in A[word]:#value olarak nerelerde tutulduğunu gösteren liste
+            print(location+1,end=" ")#yazılıyor
         print()
     else:
-        print(-1)
+        print(-1)#B deki eleman A da yoksa -1 yazılıyor
 
 print(A)
 print(B)
